d38a7df799fbe74e812889494ced1bc43f19e368
masaki <nakazawa232a@gail.com>
2024-09-02 14:45:13 +0900

    first commit to rd

---
+++ env_start.sh
@@ -0,0 +1,8 @@
+.lab_control\Scripts\activate
+
+# docker container run \
+#     -it \
+#     --privileged \
+#     --name lab_cntrl_0828 \
+#     --mount type=bind,source=/mnt/c/Users/Sakelab/Desktop/temp/2408-CELL-VAC,target=/workspace \
+#     lab_control:version2.0
